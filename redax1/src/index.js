c8943a50f8190a1eb1bf300670219d5d6ead8f96
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2022-12-02 21:52:44 +0200

    add redux dev tools ext

--- redax1/src/index.js
+++ redax1/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import reducer from './reducer';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './components/App';
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <React.StrictMode>
